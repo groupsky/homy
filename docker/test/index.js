c459e759ab12b39554b1a495ec01ea7362338e7d
Geno Roupsky <geno@roupsky.name>
2020-10-11 00:34:59 +0300

    Add logging

--- docker/test/index.js
+++ docker/test/index.js
@@ -1,16 +1,26 @@
 const mqtt = require('mqtt')
-const client = mqtt.connect(process.env.BROKER || 'mqtt://localhost')
-const timer = setTimeout(() => process.exit(1), 500)
+const brokerUrl = process.env.BROKER || 'mqtt://localhost'
+console.log(`connecting ${brokerUrl}`)
+const client = mqtt.connect(brokerUrl)
+const timer = setTimeout(() => {
+  console.error('Timeout!')
+  process.exit(1)
+}, 1000)
 
 client.on('connect', () => {
+  console.log('connected, sending...')
   client.subscribe('/homy/ard1/output', (err) => {
-    if (!err) {
-      client.publish('/homy/ard1/input', JSON.stringify({ t: 'ic', i: 22, p: 44, l: 0, v: 1 }))
+    if (err) {
+      console.log('Error sending', err)
+      process.exit(1)
     }
+    console.log(`> [${'/homy/ard1/input'}]: ${JSON.stringify({ t: 'ic', i: 22, p: 44, l: 0, v: 1 })}`)
+    client.publish('/homy/ard1/input', JSON.stringify({ t: 'ic', i: 22, p: 44, l: 0, v: 1 }))
   })
 })
 
 client.on('message', (topic, message) => {
+  console.log(`< [${topic}]: ${message}`)
   // message is Buffer
   if (topic === '/homy/ard1/output') {
     const msg = JSON.parse(message)
@@ -19,4 +29,8 @@ client.on('message', (topic, message) => {
       client.end()
     }
   }
+})
+
+client.on('error', (err) => {
+  console.log('error', err)
 })
