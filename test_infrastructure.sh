df8deb6876a3219de25023bece1f6f7b10e444f7
Geno Roupsky <geno@roupsky.name>
2020-12-26 15:52:04 +0200

    Infrastructure tests (#112)

---
+++ test_infrastructure.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eux
+
+source test.env
+
+TIMEOUT=3
+
+dc="docker-compose --env-file test.env"
+dct="$dc --file docker-compose.yml --file docker-compose.test.yml"
+vpn="$dct run test-vpn"
+ext="$dct run test-external"
+
+$dct up --build --force-recreate --no-start
+
+echo start regular containers
+$dc up --detach
+
+echo wait for ingress to boot
+timeout ${TIMEOUT}s bash -c "while ! docker inspect $INGRESS_NAME | grep '\"Status\": \"running\"' > /dev/null; do true; done"
+
+echo ingress works across vpn
+$vpn bash -c "sleep ${TIMEOUT}; curl -m ${TIMEOUT} -H 'Host: whoami.$DOMAIN' ${INGRESS_ADDRESS}"
+
+echo ingress not accessible outside vpn
+$ext sh -c "! curl -m ${TIMEOUT} -H 'Host: whoami.$DOMAIN' ${INGRESS_ADDRESS}"
+
+## TODO
+#echo vpn allows access between clients
+#$dct up --detach test-vpn2
+#timeout ${TIMEOUT}s bash -c "while ! docker inspect homy_test-vpn2_1 | grep '\"Status\": \"running\"' > /dev/null; do true; done"
+#$vpn bash -c "sleep ${TIMEOUT}; ping -c 1 -W ${TIMEOUT} ${VPN_SUBNET/%.0/.3}"
