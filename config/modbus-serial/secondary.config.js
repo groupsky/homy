01c9aef74ef8b80e1f0be77419c8676a2766bc97
Geno Roupsky <geno@roupsky.name>
2021-01-07 10:26:27 +0200

    fixup! Update modbus-serial to use secrets for mongo connection

--- config/modbus-serial/secondary.config.js
+++ config/modbus-serial/secondary.config.js
@@ -1,3 +1,30 @@
+function getFile(filePath) {
+  if (typeof filePath !== 'undefined' && filePath) {
+    const fs = require('fs');
+
+    try {
+      if (fs.existsSync(filePath)) {
+        return fs.readFileSync(filePath);
+      }
+    } catch (err) {
+      console.error('Failed to read file', filePath, err);
+    }
+  }
+  return null;
+}
+
+function getFileEnv(envVariable) {
+  const origVar = process.env[envVariable];
+  const fileVar = process.env[envVariable + '_FILE'];
+  if (fileVar) {
+    const file = getFile(fileVar);
+    if (file) {
+      return file.toString().split(/\r?\n/)[0].trim();
+    }
+  }
+  return origVar;
+}
+
 module.exports = {
   port: '/dev/ttyUSB1',
   baudRate: 9600,
@@ -52,7 +79,13 @@ module.exports = {
     },
     mongodb: {
       collection: process.env.COLLECTION,
-      url: process.env.DATABASE,
+      url: process.env.MONGODB_URL,
+      options: {
+        auth: {
+          user: getFileEnv('MONGODB_USERNAME'),
+          password: getFileEnv('MONGODB_PASSWORD'),
+        }
+      }
     },
   },
 }
