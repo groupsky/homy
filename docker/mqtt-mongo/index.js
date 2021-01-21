c106734677593b5a359bcbc44afc634647e07ec0
Geno Roupsky <geno@roupsky.name>
2021-01-21 23:54:06 +0200

    Record temperature from broadcom rm+ (#136)

--- docker/mqtt-mongo/index.js
+++ docker/mqtt-mongo/index.js
@@ -5,24 +5,43 @@ const mqtt = require('mqtt')
 
 const { MongoClient } = require('mongodb')
 const mqttUrl = process.env.BROKER
-const mongoUrl = process.env.DATABASE
+const mongoUrl = process.env.MONGODB_URL
 const collection = process.env.COLLECTION
 const topic = process.env.TOPIC
+const mongoUser = getFileEnv('MONGODB_USERNAME')
+const mongoPassword = getFileEnv('MONGODB_PASSWORD')
 
 const client = mqtt.connect(mqttUrl, {
     clientId: process.env.MQTT_CLIENT_ID
 })
 
-client.on('connect', function () {
-    console.log('connected to', mqttUrl)
-    client.subscribe(topic, function (err) {
-        if (err) {
-            console.log('Failure subscribing to topic', err)
-            process.exit(1)
+function getFile(filePath) {
+    if (filePath) {
+        const fs = require('fs');
+
+        try {
+            if (fs.existsSync(filePath)) {
+                return fs.readFileSync(filePath);
+            }
+        } catch (err) {
+            console.error('Failed to read file', filePath, err);
         }
-        console.log('subscribed to', topic)
-    })
-})
+    }
+    return null;
+}
+
+function getFileEnv(envVariable) {
+    const origVar = process.env[envVariable];
+    const fileVar = process.env[envVariable + '_FILE'];
+    if (fileVar) {
+        const file = getFile(fileVar);
+        if (file) {
+            return file.toString().split(/\r?\n/)[0].trim();
+        }
+    }
+    return origVar;
+}
+
 client.on('reconnect', function () {
     console.log('reconnected to', mqttUrl)
 })
@@ -43,19 +62,43 @@ client.on('offline', function () {
     process.exit(1)
 })
 
-MongoClient.connect(mongoUrl)
-    .then((mongoClient) => {
-        console.log('connected to', mongoUrl)
-        const db = mongoClient.db()
-        const col = db.collection(collection)
+const mongoPromise = MongoClient.connect(mongoUrl, {
+    auth: {
+        user: mongoUser,
+        password: mongoPassword
+    }
+})
 
-        client.on('message', function (topic, message) {
-            const data = JSON.parse(message)
-            col.insertOne(data, function (err, rec) {
-                if (err) {
-                    console.error('Failure writing to mongo', err)
-                    process.exit(1)
+client.on('connect', function () {
+    console.log('connected to', mqttUrl)
+    client.subscribe(topic, function (err) {
+        if (err) {
+            console.log('Failure subscribing to topic', err)
+            process.exit(1)
+        }
+        console.log('subscribed to', topic)
+
+        mongoPromise.then((mongoClient) => {
+            console.log('connected to', mongoUrl)
+            const db = mongoClient.db()
+            const col = db.collection(collection)
+
+            client.on('message', function (topic, message) {
+                const payload = JSON.parse(message)
+                if (!payload._tz) {
+                    payload._tz = Date.now()
                 }
+                const record = {
+                    topic,
+                    payload
+                }
+                col.insertOne(record, function (err) {
+                    if (err) {
+                        console.error('Failure writing to mongo', err)
+                        process.exit(1)
+                    }
+                })
             })
         })
     })
+})
