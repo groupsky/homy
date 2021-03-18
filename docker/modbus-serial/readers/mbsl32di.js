40bed5fb59ee3b75c715a1a9a82970e044b343e1
Geno Roupsky <geno@roupsky.name>
2021-03-18 21:33:16 +0200

    fixup! fixup! fixup! fixup! fixup! Implement reading from digital inputs

--- docker/modbus-serial/readers/mbsl32di.js
+++ docker/modbus-serial/readers/mbsl32di.js
@@ -2,8 +2,8 @@ module.exports = async function mbsl32di (client, { maxMsBetweenReports = 0 } = 
   const val = await client.readHoldingRegisters(0x0000, 2)
   const newFlags = val.data[1] << 16 || val.data[0]
 
-  const noChange = newFlags ^ state.flags === 0
-  const recentReport = maxMsBetweenReports === 0 || Date.now() - (state.lastReport || 0) < maxMsBetweenReports
+  const noChange = (newFlags ^ state.flags) === 0
+  const recentReport = maxMsBetweenReports === 0 || ((Date.now() - (state.lastReport || 0)) < maxMsBetweenReports)
   if (noChange && recentReport) {
     return
   }
