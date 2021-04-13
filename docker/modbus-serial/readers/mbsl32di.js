8f750b76684427a93e17becc0c378cf4af65e673
Geno Roupsky <geno@roupsky.name>
2021-04-13 21:19:26 +0300

    Fix msbl32 reading

--- docker/modbus-serial/readers/mbsl32di.js
+++ docker/modbus-serial/readers/mbsl32di.js
@@ -1,6 +1,6 @@
 module.exports = async function mbsl32di (client, { maxMsBetweenReports = 0 } = {}, state = {}) {
   const val = await client.readHoldingRegisters(0x0000, 2)
-  const newFlags = val.data[1] << 16 || val.data[0]
+  const newFlags = val.data[1] << 16 | val.data[0]
 
   const noChange = (newFlags ^ state.flags) === 0
   const recentReport = maxMsBetweenReports === 0 || ((Date.now() - (state.lastReport || 0)) < maxMsBetweenReports)
