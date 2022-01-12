9fb0d049e3698d97ad2a20482d0d1d5ffb084397
Geno Roupsky <geno@roupsky.name>
2022-01-12 18:55:32 +0200

    Refactor bots definition

--- docker/automations/bots/syncBac002Clock.js
+++ docker/automations/bots/syncBac002Clock.js
@@ -1,52 +1,21 @@
 const weekDays = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat']
 
-module.exports = (name, { topic: topicPrefix }) => {
-  console.log(`init ${name}`)
-  return ({
-    /**
-     * @param {import('mqtt').Client} mqtt
-     */
-    start: ({ gates: { mqtt } }) => {
-      console.log(`starting ${name}`)
+module.exports = (name, { topic: topicPrefix }) => ({
+  start: ({ mqtt }) => {
+    const readingTopic = `${topicPrefix}/reading`
+    const writeTopic = `${topicPrefix}/write`
 
-      const readingTopic = `${topicPrefix}/reading`
-      const writeTopic = `${topicPrefix}/write`
-
-      const pub = (payload) => mqtt.publish(writeTopic, JSON.stringify({
-        _bot: name,
-        ...payload
-      }), (err) => {
-        if (err) {
-          console.error(`failure sending to ${writeTopic}`, payload)
-          return
-        }
-      })
-
-      mqtt.on('connect', () => {
-        mqtt.subscribe(readingTopic, (err) => {
-          if (err) {
-            console.error(`failure subscribing to ${readingTopic}`, err)
-            return
-          }
-        })
-      })
-      mqtt.on('message', (topic, rawPayload) => {
-        if (topic !== readingTopic) {
-          return
-        }
-
-        const payload = JSON.parse(rawPayload.toString())
-        const now = new Date(payload._tz)
-        if (payload.minutes != null && Math.abs(payload.minutes - now.getMinutes()) > 1) {
-          pub({ minutes: new Date().getMinutes() })
-        }
-        if (payload.hours != null && payload.hours !== now.getHours()) {
-          pub({ hours: new Date().getHours() })
-        }
-        if (payload.weekDay != null && payload.weekDay !== weekDays[now.getDay()]) {
-          pub({ weekDay: weekDays[new Date().getDay()] })
-        }
-      })
-    }
-  })
-}
+    mqtt.subscribe(readingTopic, (payload) => {
+      const now = new Date(payload._tz)
+      if (payload.minutes != null && Math.abs(payload.minutes - now.getMinutes()) > 1) {
+          mqtt.publish(writeTopic, { minutes: new Date().getMinutes() })
+      }
+      if (payload.hours != null && payload.hours !== now.getHours()) {
+          mqtt.publish(writeTopic, { hours: new Date().getHours() })
+      }
+      if (payload.weekDay != null && payload.weekDay !== weekDays[now.getDay()]) {
+          mqtt.publish(writeTopic, { weekDay: weekDays[new Date().getDay()] })
+      }
+    })
+  }
+})
--- docker/automations/index.js
+++ docker/automations/index.js
@@ -14,7 +14,11 @@ const {
 const playground = {
   bots: Object.entries(botConfigs).map(([name, config]) => {
     console.log(`loading ${name}: ${config.type}`)
-    return require(`./bots/${config.type}`)(name, config)
+    return {
+      config,
+      name,
+      hooks: require(`./bots/${config.type}`)(name, config)
+    }
   }),
   gates: {
     mqtt: mqtt.connect(mqttUrl, {
@@ -24,4 +28,39 @@ const playground = {
 }
 
 console.log('starting bots')
-playground.bots.forEach(bot => bot.start(playground))
+playground.bots.forEach(bot => bot.hooks.start({
+  mqtt: {
+    subscribe: (topic, cb) => {
+      playground.gates.mqtt.on('connect', () => {
+        playground.gates.mqtt.subscribe(topic, (err) => {
+          if (err) {
+            console.error(`failure subscribing to ${topic}`, err)
+            return
+          }
+        })
+        playground.gates.mqtt.on('message', (msgTopic, payload) => {
+          if (topic !== msgTopic) {
+            return
+          }
+
+          cb(JSON.parse(payload.toString()))
+        })
+      })
+    },
+    publish: (topic, payload) => {
+      playground.gates.mqtt.publish(topic, JSON.stringify({
+        ...payload,
+        _bot: {
+          name: bot.name,
+          type: bot.config.type
+        },
+        _tz: Date.now()
+      }), (err) => {
+        if (err) {
+          console.error(`failure sending to ${topic}`, payload)
+          return
+        }
+      })
+    }
+  }
+}))
