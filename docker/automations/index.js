3bb840763fcb5b08ba94c19764deafe73d228875
Geno Roupsky <geno@roupsky.name>
2022-01-12 20:35:13 +0200

    Unify bot logging

--- docker/automations/index.js
+++ docker/automations/index.js
@@ -13,7 +13,7 @@ const {
 
 const playground = {
   bots: Object.entries(botConfigs).map(([name, config]) => {
-    console.log(`loading ${name}: ${config.type}`)
+    console.log(`[${config.type}] loading ${name}`)
     return {
       config,
       name,
@@ -27,40 +27,42 @@ const playground = {
   }
 }
 
-console.log('starting bots')
-playground.bots.forEach(bot => bot.hooks.start({
-  mqtt: {
-    subscribe: (topic, cb) => {
-      playground.gates.mqtt.on('connect', () => {
-        playground.gates.mqtt.subscribe(topic, (err) => {
-          if (err) {
-            console.error(`failure subscribing to ${topic}`, err)
-            return
-          }
-        })
-        playground.gates.mqtt.on('message', (msgTopic, payload) => {
-          if (topic !== msgTopic) {
-            return
-          }
+playground.bots.forEach(bot => {
+  console.log(`[${bot.config.type}] starting ${bot.name}`)
+  bot.hooks.start({
+    mqtt: {
+      subscribe: (topic, cb) => {
+        playground.gates.mqtt.on('connect', () => {
+          playground.gates.mqtt.subscribe(topic, (err) => {
+            if (err) {
+              console.error(`[${bot.config.type}] failure subscribing to ${topic}`, err)
+              return
+            }
+          })
+          playground.gates.mqtt.on('message', (msgTopic, payload) => {
+            if (topic !== msgTopic) {
+              return
+            }
 
-          cb(JSON.parse(payload.toString()))
+            cb(JSON.parse(payload.toString()))
+          })
         })
-      })
-    },
-    publish: (topic, payload) => {
-      playground.gates.mqtt.publish(topic, JSON.stringify({
-        ...payload,
-        _bot: {
-          name: bot.name,
-          type: bot.config.type
-        },
-        _tz: Date.now()
-      }), (err) => {
-        if (err) {
-          console.error(`failure sending to ${topic}`, payload)
-          return
-        }
-      })
+      },
+      publish: (topic, payload) => {
+        playground.gates.mqtt.publish(topic, JSON.stringify({
+          ...payload,
+          _bot: {
+            name: bot.name,
+            type: bot.config.type
+          },
+          _tz: Date.now()
+        }), (err) => {
+          if (err) {
+            console.error(`[${bot.config.type}] failure sending to ${topic}`, payload)
+            return
+          }
+        })
+      }
     }
-  }
-}))
+  })
+})
