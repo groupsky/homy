b31194e0f487d19c0df7cabdb7fab25e96269f01
Geno Roupsky <geno@roupsky.name>
2022-03-08 19:49:59 +0200

    feat(bots): integrate binary_switches in ha through discovery

--- config/automations/ha_discovery.js
+++ config/automations/ha_discovery.js
@@ -8,6 +8,7 @@ const areas = {
   bath3: 'Баня 3',
   bedroom: 'Спалня',
   boris: 'Борис',
+  cabinet: 'Кабинет',
   corridor1: 'Коридор 1',
   corridor2: 'Коридор 2',
   external: 'Вън',
@@ -62,13 +63,48 @@ const haLight = (
   }
 })
 
-module.exports = {
+/**
+ * @param {string} name
+ * @param {string} feature
+ * @param {('door'|'lock'|'window')} device_class
+ * @param {object} config
+ * @param {string} feature_type
+ * @return {Object}
+ */
+const haBinarySensor = (
+  {
+    name,
+    feature,
+    type: device_class,
+    config,
+    feature_type = device_class === 'lock' ? 'lock' : 'open'
+  }
+) => ({
+  [`${name}HaDiscovery`]: {
+    type: 'transform',
+    input: {
+      name: 'inputs/static', params: {
+        payload: {
+          name,
+          device_class,
+          state_topic: `${featuresPrefix}/${feature_type}/${feature}/status`,
+          value_template: '{{ \'ON\' if value_json.state else \'OFF\' }}',
+          ...config
+        }
+      }
+    },
+    transform: [],
+    output: { name: 'outputs/mqtt', params: { topic: `${haPrefix}/binary_sensor/${feature}/config`, retain: true } }
+  }
+})
+
+const config = {
   bots: {
     ...haLight({
       name: 'antreCeilingLight',
       feature: 'antre_ceiling_light',
       config: {
-        name: 'Таван',
+        name: 'Антре',
         device: devices.antre,
         object_id: 'antre_ceiling_light',
         unique_id: 'homy_antre_ceiling_light',
@@ -78,7 +114,7 @@ module.exports = {
       name: 'bath2CeilingLight',
       feature: 'bath2_ceiling_light',
       config: {
-        name: 'Таван',
+        name: 'Баня 2',
         device: devices.bath2,
         object_id: 'bath2_ceiling_light',
         unique_id: 'homy_bath2_ceiling_light',
@@ -88,7 +124,7 @@ module.exports = {
       name: 'bedroomCeilingLight',
       feature: 'bedroom_ceiling_light',
       config: {
-        name: 'Таван',
+        name: 'Спалня',
         device: devices.bedroom,
         object_id: 'bedroom_ceiling_light',
         unique_id: 'homy_bedroom_ceiling_light',
@@ -98,7 +134,7 @@ module.exports = {
       name: 'martinCeilingLight',
       feature: 'martin_ceiling_light',
       config: {
-        name: 'Таван',
+        name: 'Мартин',
         device: devices.martin,
         object_id: 'martin_ceiling_light',
         unique_id: 'homy_martin_ceiling_light',
@@ -108,7 +144,7 @@ module.exports = {
       name: 'bath1CeilingLight',
       feature: 'bath1_ceiling_light',
       config: {
-        name: 'Таван',
+        name: 'Баня 1',
         device: devices.bath1,
         object_id: 'bath1_ceiling_light',
         unique_id: 'homy_bath1_ceiling_light',
@@ -118,8 +154,8 @@ module.exports = {
       name: 'officeCeilingLight',
       feature: 'office_ceiling_light',
       config: {
-        name: 'Таван',
-        device: devices.bedroom,
+        name: 'Кабинет',
+        device: devices.cabinet,
         object_id: 'office_ceiling_light',
         unique_id: 'homy_office_ceiling_light',
       }
@@ -128,7 +164,7 @@ module.exports = {
       name: 'bedroomBedLeftLight',
       feature: 'bedroom_bed_left_light',
       config: {
-        name: 'Лява',
+        name: 'Спалня лява',
         device: devices.bedroom,
         object_id: 'bedroom_bed_left_light',
         unique_id: 'homy_bedroom_bed_left_light',
@@ -138,7 +174,7 @@ module.exports = {
       name: 'verandaWestLight',
       feature: 'veranda_west_light',
       config: {
-        name: 'Запад',
+        name: 'Веранда запад',
         device: devices.external,
         object_id: 'veranda_west_light',
         unique_id: 'homy_veranda_west_light',
@@ -148,7 +184,7 @@ module.exports = {
       name: 'gerganaCeilingLight',
       feature: 'gergana_ceiling_light',
       config: {
-        name: 'Таван',
+        name: 'Гергана',
         device: devices.gergana,
         object_id: 'gergana_ceiling_light',
         unique_id: 'homy_gegana_ceiling_light',
@@ -158,7 +194,7 @@ module.exports = {
       name: 'borisCeilingLight',
       feature: 'boris_ceiling_light',
       config: {
-        name: 'Таван',
+        name: 'Борис',
         device: devices.boris,
         object_id: 'boris_ceiling_light',
         unique_id: 'homy_boris_ceiling_light',
@@ -168,7 +204,7 @@ module.exports = {
       name: 'laundryCeilingLight',
       feature: 'laundry_ceiling_light',
       config: {
-        name: 'Таван',
+        name: 'Мокро',
         device: devices.laundry,
         object_id: 'laundry_ceiling_light',
         unique_id: 'homy_laundry_ceiling_light',
@@ -178,7 +214,7 @@ module.exports = {
       name: 'corridor2BothCeilingLights',
       feature: 'corridor2_both_ceiling_lights',
       config: {
-        name: 'Таван',
+        name: 'Коридор 2',
         device: devices.corridor2,
         object_id: 'corridor2_both_ceiling_lights',
         unique_id: 'homy_corridor2_both_ceiling_lights',
@@ -188,7 +224,7 @@ module.exports = {
       name: 'kitchenAllCeilingLights',
       feature: 'kitchen_all_ceiling_lights',
       config: {
-        name: 'Таван',
+        name: 'Кухня',
         device: devices.kitchen,
         object_id: 'kitchen_all_ceiling_lights',
         unique_id: 'homy_kitchen_all_ceiling_lights',
@@ -198,7 +234,7 @@ module.exports = {
       name: 'corridor1CeilingLight',
       feature: 'corridor1_ceiling_light',
       config: {
-        name: 'Таван',
+        name: 'Коридор 1',
         device: devices.corridor1,
         object_id: 'corridor1_ceiling_light',
         unique_id: 'homy_corridor1_ceiling_light',
@@ -208,11 +244,452 @@ module.exports = {
       name: 'livingroomCeilingLight',
       feature: 'livingroom_ceiling_light',
       config: {
-        name: 'Таван',
+        name: 'Хол',
         device: devices.livingroom,
         object_id: 'livingroom_ceiling_light',
         unique_id: 'homy_livingroom_ceiling_light',
       }
+    }),
+
+    ...haBinarySensor({
+      name: 'frontMainDoorOpen',
+      feature_type: 'open',
+      feature: 'front_main_door_open',
+      type: 'door',
+      config: {
+        name: 'Входна',
+        device: devices.antre,
+        object_id: 'front_main_door_open',
+        unique_id: 'homy_front_main_door_open',
+      },
+    }),
+    ...haBinarySensor({
+      name: 'frontSideDoorOpen',
+      feature_type: 'open',
+      feature: 'front_side_door_open',
+      type: 'door',
+      config: {
+        name: 'Помощна',
+        device: devices.antre,
+        object_id: 'front_side_door_open',
+        unique_id: 'homy_front_side_door_open',
+      },
+    }),
+    ...haBinarySensor({
+      name: 'livingWindowOpen',
+      feature_type: 'open',
+      feature: 'living_window_open',
+      type: 'window',
+      config: {
+        name: 'Хол запад',
+        device: devices.livingroom,
+        object_id: 'living_window_open',
+        unique_id: 'homy_living_window_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'cabinetSouthWindowOpen',
+      feature_type: 'open',
+      feature: 'cabinet_south_window_open',
+      type: 'window',
+      config: {
+        name: 'Кабинет запад',
+        device: devices.cabinet,
+        object_id: 'cabinet_south_window_open',
+        unique_id: 'homy_cabinet_south_window_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'cabinetWestWindowOpen',
+      feature_type: 'open',
+      feature: 'cabinet_west_window_open',
+      type: 'window',
+      config: {
+        name: 'Кабинет юг',
+        device: devices.cabinet,
+        object_id: 'cabinet_west_window_open',
+        unique_id: 'homy_cabinet_west_window_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'bath1DoorLock',
+      feature_type: 'lock',
+      feature: 'bath1_door_lock',
+      type: 'lock',
+      config: {
+        name: 'Баня 1',
+        device: devices.bath1,
+        object_id: 'bath1_door_lock',
+        unique_id: 'homy_bath1_door_lock',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'bath1DoorOpen',
+      feature_type: 'open',
+      feature: 'bath1_door_open',
+      type: 'door',
+      config: {
+        name: 'Баня 1',
+        device: devices.bath1,
+        object_id: 'bath1_door_open',
+        unique_id: 'homy_bath1_door_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'cabinetDoorLock',
+      feature_type: 'lock',
+      feature: 'cabinet_door_lock',
+      type: 'lock',
+      config: {
+        name: 'Кабинет',
+        device: devices.cabinet,
+        object_id: 'cabinet_door_lock',
+        unique_id: 'homy_cabinet_door_lock',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'cabinetDoorOpen',
+      feature_type: 'open',
+      feature: 'cabinet_door_open',
+      type: 'door',
+      config: {
+        name: 'Кабинет',
+        device: devices.cabinet,
+        object_id: 'cabinet_door_open',
+        unique_id: 'homy_cabinet_door_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'bath2DoorLock',
+      feature_type: 'lock',
+      feature: 'bath2_door_lock',
+      type: 'lock',
+      config: {
+        name: 'Баня 2',
+        device: devices.bath2,
+        object_id: 'bath2_door_lock',
+        unique_id: 'homy_bath2_door_lock',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'bath2DoorOpen',
+      feature_type: 'open',
+      feature: 'bath2_door_open',
+      type: 'door',
+      config: {
+        name: 'Баня 2',
+        device: devices.bath2,
+        object_id: 'bath2_door_open',
+        unique_id: 'homy_bath2_door_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'bath3DoorLock',
+      feature_type: 'lock',
+      feature: 'bath3_door_lock',
+      type: 'lock',
+      config: {
+        name: 'Баня 3',
+        device: devices.bath3,
+        object_id: 'bath3_door_lock',
+        unique_id: 'homy_bath3_door_lock',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'bath3DoorOpen',
+      feature_type: 'open',
+      feature: 'bath3_door_open',
+      type: 'door',
+      config: {
+        name: 'Баня 3',
+        device: devices.bath3,
+        object_id: 'bath3_door_open',
+        unique_id: 'homy_bath3_door_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'gerganaDoorLock',
+      feature_type: 'lock',
+      feature: 'gergana_door_lock',
+      type: 'lock',
+      config: {
+        name: 'Гергана',
+        device: devices.gergana,
+        object_id: 'gergana_door_lock',
+        unique_id: 'homy_gergana_door_lock',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'gerganaDoorOpen',
+      feature_type: 'open',
+      feature: 'gergana_door_open',
+      type: 'door',
+      config: {
+        name: 'Гергана',
+        device: devices.gergana,
+        object_id: 'gergana_door_open',
+        unique_id: 'homy_gergana_door_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'bedroomDoorLock',
+      feature_type: 'lock',
+      feature: 'bedroom_door_lock',
+      type: 'lock',
+      config: {
+        name: 'Спалня',
+        device: devices.bedroom,
+        object_id: 'bedroom_door_lock',
+        unique_id: 'homy_bedroom_door_lock',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'bedroomDoorOpen',
+      feature_type: 'open',
+      feature: 'bedroom_door_open',
+      type: 'door',
+      config: {
+        name: 'Спалня',
+        device: devices.bedroom,
+        object_id: 'bedroom_door_open',
+        unique_id: 'homy_bedroom_door_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'borisDoorLock',
+      feature_type: 'lock',
+      feature: 'boris_door_lock',
+      type: 'lock',
+      config: {
+        name: 'Борис',
+        device: devices.boris,
+        object_id: 'boris_door_lock',
+        unique_id: 'homy_boris_door_lock',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'borisDoorOpen',
+      feature_type: 'open',
+      feature: 'boris_door_open',
+      type: 'door',
+      config: {
+        name: 'Борис',
+        device: devices.boris,
+        object_id: 'boris_door_open',
+        unique_id: 'homy_boris_door_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'bath1WindowOpen',
+      feature_type: 'open',
+      feature: 'bath1_window_open',
+      type: 'window',
+      config: {
+        name: 'Баня 1',
+        device: devices.bath1,
+        object_id: 'bath1_window_open',
+        unique_id: 'homy_bath1_window_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'martinDoorLock',
+      feature_type: 'lock',
+      feature: 'martin_door_lock',
+      type: 'lock',
+      config: {
+        name: 'Мартин',
+        device: devices.martin,
+        object_id: 'martin_door_lock',
+        unique_id: 'homy_martin_door_lock',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'martinDoorOpen',
+      feature_type: 'open',
+      feature: 'martin_door_open',
+      type: 'door',
+      config: {
+        name: 'Мартин',
+        device: devices.martin,
+        object_id: 'martin_door_open',
+        unique_id: 'homy_martin_door_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'serviceInternalDoorLock',
+      feature_type: 'lock',
+      feature: 'service_internal_door_lock',
+      type: 'lock',
+      config: {
+        name: 'Вътрешна',
+        device: devices.service,
+        object_id: 'service_internal_door_lock',
+        unique_id: 'homy_service_internal_door_lock',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'serviceInternalDoorOpen',
+      feature_type: 'open',
+      feature: 'service_internal_door_open',
+      type: 'door',
+      config: {
+        name: 'Вътрешна',
+        device: devices.service,
+        object_id: 'service_internal_door_open',
+        unique_id: 'homy_service_internal_door_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'kitchenWindowOpen',
+      feature_type: 'open',
+      feature: 'kitchen_window_open',
+      type: 'window',
+      config: {
+        name: 'Кухня',
+        device: devices.kitchen,
+        object_id: 'kitchen_window_open',
+        unique_id: 'homy_kitchen_window_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'bath2WindowOpen',
+      feature_type: 'open',
+      feature: 'bath2_window_open',
+      type: 'window',
+      config: {
+        name: 'Баня 2',
+        device: devices.bath2,
+        object_id: 'bath2_window_open',
+        unique_id: 'homy_bath2_window_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'bedroomExternalDoorOpen',
+      feature_type: 'open',
+      feature: 'bedroom_external_door_open',
+      type: 'door',
+      config: {
+        name: 'Спалня тераса',
+        device: devices.bedroom,
+        object_id: 'bedroom_external_door_open',
+        unique_id: 'homy_bedroom_external_door_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'bedroomWindowOpen',
+      feature_type: 'open',
+      feature: 'bedroom_window_open',
+      type: 'window',
+      config: {
+        name: 'Спалня',
+        device: devices.bedroom,
+        object_id: 'bedroom_window_open',
+        unique_id: 'homy_bedroom_window_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'martinWestWindowOpen',
+      feature_type: 'open',
+      feature: 'martin_west_window_open',
+      type: 'window',
+      config: {
+        name: 'Мартин запад',
+        device: devices.martin,
+        object_id: 'martin_west_window_open',
+        unique_id: 'homy_martin_west_window_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'martinSouthWindowOpen',
+      feature_type: 'open',
+      feature: 'martin_south_window_open',
+      type: 'window',
+      config: {
+        name: 'Мартин юг',
+        device: devices.martin,
+        object_id: 'martin_south_window_open',
+        unique_id: 'homy_martin_south_window_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'gerganaExternalDoorOpen',
+      feature_type: 'open',
+      feature: 'gergana_external_door_open',
+      type: 'door',
+      config: {
+        name: 'Гергана тераса',
+        device: devices.gergana,
+        object_id: 'gergana_external_door_open',
+        unique_id: 'homy_gergana_external_door_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'borisExternalDoorOpen',
+      feature_type: 'open',
+      feature: 'boris_external_door_open',
+      type: 'door',
+      config: {
+        name: 'Борис тераса',
+        device: devices.boris,
+        object_id: 'boris_external_door_open',
+        unique_id: 'homy_boris_external_door_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'gerganaWindowOpen',
+      feature_type: 'open',
+      feature: 'gergana_window_open',
+      type: 'window',
+      config: {
+        name: 'Гергана',
+        device: devices.gergana,
+        object_id: 'gergana_window_open',
+        unique_id: 'homy_gergana_window_open',
+      },
+
+    }),
+    ...haBinarySensor({
+      name: 'borisWindowOpen',
+      feature_type: 'open',
+      feature: 'boris_window_open',
+      type: 'window',
+      config: {
+        name: 'Борис',
+        device: devices.boris,
+        object_id: 'boris_window_open',
+        unique_id: 'homy_boris_window_open',
+      },
+
     }),
   },
   gates: {
@@ -222,3 +699,5 @@ module.exports = {
     },
   }
 }
+
+module.exports = config
