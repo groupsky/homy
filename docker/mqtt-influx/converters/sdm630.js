18b19390c2b0f3b77afa9510bc7cb5a29c288bec
Geno Roupsky <geno@roupsky.name>
2023-10-16 20:48:43 +0300

    process sdm630 in mqtt-influx

---
+++ docker/mqtt-influx/converters/sdm630.js
@@ -0,0 +1,56 @@
+const {Point} = require('@influxdata/influxdb-client')
+
+module.exports = function sdm630(input) {
+    return [
+        new Point('current_power')
+            .tag('device.name', input.device)
+            .tag('device.type', input._type)
+            .tag('device.addr', input._addr)
+            .tag('phase', 'A')
+            .timestamp(input._tz)
+            .floatField('v', input.av)
+            .floatField('c', input.ac)
+            .floatField('p', input.a_ap),
+
+        new Point('current_power')
+            .tag('device.name', input.device)
+            .tag('device.type', input._type)
+            .tag('device.addr', input._addr)
+            .tag('phase', 'B')
+            .timestamp(input._tz)
+            .floatField('v', input.bv)
+            .floatField('c', input.bc)
+            .floatField('p', input.b_ap),
+
+        new Point('current_power')
+            .tag('device.name', input.device)
+            .tag('device.type', input._type)
+            .tag('device.addr', input._addr)
+            .tag('phase', 'C')
+            .timestamp(input._tz)
+            .floatField('v', input.cv)
+            .floatField('c', input.cc)
+            .floatField('p', input.c_ap),
+
+        new Point('power_meter')
+            .tag('device.name', input.device)
+            .tag('device.type', input._type)
+            .tag('device.addr', input._addr)
+            .timestamp(input._tz)
+            .floatField('total', input.total_kwh),
+
+        new Point('power_meter')
+            .tag('device.name', input.device)
+            .tag('device.type', input._type)
+            .tag('device.addr', input._addr)
+            .timestamp(input._tz)
+            .floatField('total', input.total_import_kwh),
+
+        new Point('power_meter')
+            .tag('device.name', input.device)
+            .tag('device.type', input._type)
+            .tag('device.addr', input._addr)
+            .timestamp(input._tz)
+            .floatField('total_export', input.total_export_kwh),
+    ]
+}
