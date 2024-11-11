ca7cc70dae5d882682483b610b134e47157361f6
Geno Roupsky <geno@roupsky.name>
2024-11-11 19:10:07 +0200

    add setup to xymd01

--- docker/modbus-serial/devices/xymd1.js
+++ docker/modbus-serial/devices/xymd1.js
@@ -1,3 +1,14 @@
+// Default configuration for XY-MD1 device:
+// address: 1
+// baudRate: 9600
+// parity: even
+// stopBits: 1
+// dataBits: 8
+
+/**
+ * @param {import('modbus-serial').ModbusRTU} client
+ * @return {Promise<{t: number, h: number}>}
+ */
 async function read (client) {
   const val = await client.readInputRegisters(1, 2)
 
@@ -9,6 +20,27 @@ async function read (client) {
   return { t, h }
 }
 
+/**
+ * Setup communication parameters - changes are applied after device restart
+ * @param {import('modbus-serial').ModbusRTU} client
+ * @param {{
+ *   [address]: number,
+ *   [baudRate]: number,
+ * }} newConfig
+ * @return {Promise<void>}
+ */
+async function setup (client, newConfig) {
+  if (newConfig.address != null) {
+    await client.writeRegisters(0x101, [newConfig.address])
+    client.setID(newConfig.address)
+  }
+  if (newConfig.baudRate != null) {
+    await client.writeRegisters(0x102, [newConfig.baudRate])
+  }
+}
+
+
 module.exports = {
-  read
+  read,
+  setup
 }
