2d9626be3c082d1f50dd95ac2220f5c333df492d
Geno Roupsky <geno@roupsky.name>
2024-11-17 10:50:13 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fix lights test

--- docker/test/index.js
+++ docker/test/index.js
@@ -6,7 +6,6 @@ const timer = setTimeout(() => {
     console.error('Timeout!')
     process.exit(1)
 }, 2000)
-let interval = null
 
 const publish = (topic, payload) => {
     console.log(`> [${topic}]: ${JSON.stringify(payload)}`)
@@ -15,39 +14,39 @@ const publish = (topic, payload) => {
 
 client.on('connect', () => {
     console.log('connected, sending...')
-    client.subscribe('#', async (err) => {
+    client.subscribe(['#'], async (err) => {
         if (err) {
             console.log('Error sending', err)
             process.exit(1)
         }
-        let inputs = 0
-        setInterval(() => {
-            inputs = inputs === 0 ? 1 << 27 : 0
-            publish('/modbus/dry-switches/mbsl32di2/reading', JSON.stringify({
-                "inputs": inputs,
-                "_tz": Date.now(),
-                "_ms": 7,
-                "_addr": 32,
-                "_type": "mbsl32di",
-                "device": "mbsl32di2"
-            }))
-        }, 100)
+        await publish('/modbus/dry-switches/mbsl32di2/reading', {
+            "inputs": 0,
+            "_tz": Date.now(),
+            "_ms": 7,
+            "_addr": 32,
+            "_type": "mbsl32di",
+            "device": "mbsl32di2"
+        })
+        await publish('/modbus/dry-switches/mbsl32di2/reading', {
+            "inputs": 1 << 27,
+            "_tz": Date.now(),
+            "_ms": 7,
+            "_addr": 32,
+            "_type": "mbsl32di",
+            "device": "mbsl32di2"
+        })
     })
 })
 
 client.on('message', (topic, message) => {
     console.log(`< [${topic}]: ${message}`)
     // message is Buffer
-    switch (topic) {
-        case '/modbus/dry-switches/relays00-15/write': {
-            const msg = JSON.parse(message)
-            if (msg.out8 === true) {
-                clearTimeout(timer)
-                clearInterval(interval)
-                client.end()
-            }
+    if (topic === '/modbus/dry-switches/relays00-15/write') {
+        const msg = JSON.parse(message)
+        if (msg.out8 === true) {
+            clearTimeout(timer)
+            client.end()
         }
-        break
     }
 })
 
